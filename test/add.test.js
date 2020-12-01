7680dd9bf82eb08e01e128a1cff61bb1f1bf68f6
Joonas Pelttari <joonas.pelttari@tuni.fi>
2020-12-01 17:04:06 +0200

    gitignore and add.test.js update

--- test/add.test.js
+++ test/add.test.js
@@ -6,11 +6,53 @@ import add from '../src/add.js';
 
 describe('Add.js', () => {
 
-    it('should return correct sum', () => {
-        const number1 = 5;
-        const number2 = 7;
+    it('should return correct sum with two positive numbers', () => {
+        const number1 = Math.floor(Math.random() * 100);
+        const number2 = Math.floor(Math.random() * 100);
         const sum = add(number1, number2);
         expect(sum).to.equal(number1 + number2);
+      });
+
+      it('should return correct sum with two negative numbers', () => {
+        const number1 = -(Math.floor(Math.random() * 100));
+        const number2 = -(Math.floor(Math.random() * 100));
+        const sum = add(number1, number2);
+        expect(sum).to.equal(number1 + number2);
+      });
+
+      it('should return correct sum with positive and negative number', () => {
+        const number1 = Math.floor(Math.random() * 100);
+        const number2 = -(Math.floor(Math.random() * 100));
+        const sum = add(number1, number2);
+        expect(sum).to.equal(number1 + number2);
+      });
+
+      it('should return correct sum with zero', () => {
+        const number1 = Math.floor(Math.random() * 100);
+        const number2 = 0;
+        const sum = add(number1, number2);
+        expect(sum).to.equal(number1 + number2);
+      });
+
+      it('MAX_SAFE_INTEGER + 100 should NOT return MAX_SAFE_INTEGER', () => {
+        const number1 = Number.MAX_SAFE_INTEGER;
+        const number2 = 100;
+        const sum = add(number1, number2);
+        expect(sum).to.not.equal(number1);
+      });
+
+      it('should NOT add number 100 and string "Word"', () => {
+        const number1 = 100;
+        const number2 = "Word";
+        const sum = add(number1, number2);
+        expect(sum).to.not.equal(number1 + number2);
+      });
+
+      it('should NOT add number and array', () => {
+        const number1 = 100;
+        const number2 = [];
+        const sum = add(number1, number2);
+        expect(sum).to.not.equal(number1 + number2);
       });
 
 });
