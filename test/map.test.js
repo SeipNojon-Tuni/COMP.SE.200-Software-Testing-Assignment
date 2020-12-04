1467bf97e8a44b24dd03d7ea3e827b4f4eb96c16
Sipi Ylä-Nojonen <sipi.yla-nojonen@tuni.fi>
2020-12-04 23:12:19 +0200

    Added tests for map.js

---
+++ test/map.test.js
@@ -0,0 +1,33 @@
+import chai from "chai";
+import map from "../src/map.js";
+
+describe("", () => {
+  it("Should map array values ", () => {
+    const square = (n) => { return Math.pow(n, 2) };
+    chai.expect(map([2, 3, 4, 8], square )).to.eql([4, 9, 16, 64]);
+
+    const double = (n) => { return 2*n }
+    chai.expect( map([2, 3, 4, 8], double)).to.eql([4, 6, 8, 16]);
+  });
+
+  it("Should return empty array unchanged", () => {
+    const double = (n) => { return 2*n }
+    chai.expect([], double).to.eql([]);
+  });
+
+  it("Should throw error when parameter is not array", () => {
+    const double = (n) => { return 2*n }
+
+
+    chai.expect(map("not an array!", double)).to.eql("");
+
+    chai.expect(() => {map(undefined, double)}).to.throw();
+    chai.expect(() => {map("not an array!", double)}).to.throw();
+  });
+
+  it("Should throw error when iteratee is not defined or not a function", () => {
+    chai.expect(() => {map([1, 2, 3], undefined)}).to.throw();
+    chai.expect(() => {map([1, 2, 3], "not a function!")}).to.throw();
+  });
+
+});
