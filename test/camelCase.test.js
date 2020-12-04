061617cc4128041b97c708e8b84f18ae56b6e279
Joonas Pelttari <joonas.pelttari@tuni.fi>
2020-12-04 13:52:22 +0200

    Added positive tests for camelCase.js

---
+++ test/camelCase.test.js
@@ -0,0 +1,24 @@
+import chai from 'chai';
+import camelCase from '../src/camelCase.js';
+
+describe('camelCase.js', () => {
+
+    it('Should return "fooBar" for "Foo Bar"', () => {
+        const testParam = 'Foo Bar';
+        const result = camelCase(testParam);
+        chai.expect(result).to.equal('fooBar');
+      });
+
+    it('Should return "fooBar" for "--foo-bar--"', () => {
+        const testParam = '--foo-bar--';
+        const result = camelCase(testParam);
+        chai.expect(result).to.equal('fooBar');
+      });
+
+    it('Should return "fooBar" for "__FOO_BAR__"', () => {
+        const testParam = '__FOO_BAR__';
+        const result = camelCase(testParam);
+        chai.expect(result).to.equal('fooBar');
+      });
+
+});
