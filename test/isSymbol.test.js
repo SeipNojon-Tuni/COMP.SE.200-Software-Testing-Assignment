430e4136167397b566449c856ba20ff314b10161
Sipi Ylä-Nojonen <sipi.yla-nojonen@tuni.fi>
2020-12-05 05:57:23 +0200

    Added tests for isSymbol.js

---
+++ test/isSymbol.test.js
@@ -0,0 +1,15 @@
+import chai from "chai";
+import isSymbol from "../src/isSymbol.js";
+
+describe("Is Symbol", () => {
+  it("Should return true for symbols", () => {
+    chai.expect(isSymbol(Symbol())).to.equal(true);
+    chai.expect(isSymbol(Symbol("asd"))).to.equal(true);
+    chai.expect(isSymbol(Symbol(()=>{}))).to.equal(true);
+  });
+  it("Should return false when value is not a symbol", () => {
+    chai.expect(isSymbol(123)).to.equal(false);
+    chai.expect(isSymbol([])).to.equal(false);
+    chai.expect(isSymbol("Not symbol")).to.equal(false);
+  });
+});
