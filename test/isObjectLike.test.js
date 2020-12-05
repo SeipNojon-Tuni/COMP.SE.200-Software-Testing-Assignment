933a61a922d474c6e4dd7b3e32cfa7f672d4bd4d
Sipi Ylä-Nojonen <sipi.yla-nojonen@tuni.fi>
2020-12-05 06:07:09 +0200

    Added tests for isObjectLike.js

---
+++ test/isObjectLike.test.js
@@ -0,0 +1,16 @@
+import chai from "chai";
+import isObjectLike from "../src/isObjectLike.js";
+
+describe("Is Object-like", () => {
+  it("Should return true for object-like values", () => {
+    chai.expect(isObjectLike({})).to.equal(true);
+    chai.expect(isObjectLike([])).to.equal(true);
+
+  });
+  it("Should return false for non-object-like values", () => {
+    chai.expect(isObjectLike(undefined)).to.equal(false);
+    chai.expect(isObjectLike(null)).to.equal(false);
+    chai.expect(isObjectLike("Object-like")).to.equal(false);
+    chai.expect(isObjectLike(777)).to.equal(false);
+  });
+});
