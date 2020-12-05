d4c032a5af4c67d6bac6d05d2f6f22be80a22657
Sipi Ylä-Nojonen <sipi.yla-nojonen@tuni.fi>
2020-12-05 04:43:38 +0200

    added tests for keys.js

---
+++ test/keys.test.js
@@ -0,0 +1,18 @@
+import chai from "chai";
+import keys from "../src/keys.js";
+
+describe("Keys", () => {
+  it("Should return keys for object", () => {
+    chai.expect(keys({"a":1, "b":2, "c":4 })).to.eql(["a", "b", "c"]);
+  });
+  it("Should return indexes for string and array", () => {
+    chai.expect(keys("hello")).to.eql(["0", "1", "2", "3", "4"]);
+  });
+  it("Should return empty array for empty array or object", () => {
+    chai.expect(keys([])).to.eql([]);
+    chai.expect(keys({})).to.eql([]);
+  });
+  it("Should return empty array for undefined parameter", () => {
+    chai.expect(keys(undefined)).to.eql([])
+  });
+});
