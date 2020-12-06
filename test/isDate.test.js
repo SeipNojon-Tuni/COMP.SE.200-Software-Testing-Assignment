a8cff2c4831b24b9d2c0e4e1b81ab2e8660b81f0
Sipi Ylä-Nojonen <sipi.yla-nojonen@tuni.fi>
2020-12-06 20:11:42 +0200

    Added tests for isDate.js

---
+++ test/isDate.test.js
@@ -0,0 +1,15 @@
+import chai from "chai";
+import isDate from "../src/isDate.js";
+
+describe("Is Date", () => {
+  it("Should return true for Date objects", () => {
+    chai.expect(isDate(new Date)).to.equal(true);
+  });
+  // Having null as separate negative test case is
+  // redundant as null should be considered just 'not a Date'
+  it("Should return false for values that are not Date objects", () => {
+    chai.expect(isDate(null)).to.equal(false);
+    chai.expect(isDate("Not a date")).to.equal(false);
+    chai.expect(isDate('13.2.2020')).to.equal(false);
+  });
+});
