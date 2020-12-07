e9d715282214e893a1be2571b76c1b50b6e4a3c5
Joonas Pelttari <joonas.pelttari@tuni.fi>
2020-12-07 02:13:01 +0200

    Added tests for clamp.js

---
+++ test/clamp.test.js
@@ -0,0 +1,46 @@
+import chai from "chai";
+import clamp from "../src/clamp.js";
+
+describe("clamp", () => {
+  //Positive tests:
+  it('Should return -5 for (-10, -5, 5)', () => {
+    const result = clamp(-10, -5, 5);
+    chai.expect(result).to.equal(-5);
+  });
+
+  it('Should return -5 for (-10000000, -5, 5)', () => {
+    const result = clamp(-10000000, -5, 5);
+    chai.expect(result).to.equal(-5);
+  });
+
+  it('Should return 5 for (10, -5, 5)', () => {
+    const result = clamp(10, -5, 5);
+    chai.expect(result).to.equal(5);
+  });
+
+  it('Should return 5 for (10000000, -5, 5)', () => {
+    const result = clamp(10000000, -5, 5);
+    chai.expect(result).to.equal(5);
+  });
+
+  //Negative tests:
+  it('Should return -5 if lower and upper bound are vice versa', () => {
+    const result = clamp(-10, 5, -5);
+    chai.expect(result).to.equal(-5);
+  });
+
+  it('Should return Nan when the first parameter is a string', () => {
+    const result = clamp("TestString", -5, 5);
+    chai.expect(result).to.deep.equal(NaN);
+  });
+
+  it('Should return Nan when the second parameter is a string', () => {
+    const result = clamp(10, "TestString", 5);
+    chai.expect(result).to.deep.equal(NaN);
+  });
+
+  it('Should return Nan when the third parameter is a string', () => {
+    const result = clamp(10, -5, "TestString");
+    chai.expect(result).to.deep.equal(NaN);
+  });
+});
